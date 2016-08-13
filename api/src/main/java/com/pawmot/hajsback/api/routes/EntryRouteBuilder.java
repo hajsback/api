75a5d345e2a6d786b5b318782b5eba199c5c5d27
Paweł Motyl <pawmot@gmail.com>
2016-08-13 14:59:04 +0200

    API Gateway pushes requests to JMS

--- api/src/main/java/com/pawmot/hajsback/api/routes/EntryRouteBuilder.java
+++ api/src/main/java/com/pawmot/hajsback/api/routes/EntryRouteBuilder.java
@@ -1,5 +1,6 @@
 package com.pawmot.hajsback.api.routes;
 
+import org.apache.camel.Processor;
 import org.apache.camel.spring.SpringRouteBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,17 +8,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Component
 public class EntryRouteBuilder extends SpringRouteBuilder {
     public static final String ENTRY_ROUTE_ENDPOINT = "direct:entry";
+    private static final Processor log;
+
+    static {
+        log = ex -> {
+            String httpRoute = ex.getIn().getHeader("HttpRoute", String.class);
+            RequestMethod method = ex.getIn().getHeader("HttpMethod", RequestMethod.class);
+            String body = ex.getIn().getBody(String.class);
+
+            System.out.println(String.format("%s %s - %s\n", method, httpRoute, body));
+        };
+    }
 
     @Override
     public void configure() throws Exception {
         from(ENTRY_ROUTE_ENDPOINT).routeId("entry")
-                .process(ex -> {
-                    String httpRoute = ex.getIn().getHeader("HttpRoute", String.class);
-                    RequestMethod method = ex.getIn().getHeader("HttpMethod", RequestMethod.class);
-                    String body = ex.getIn().getBody(String.class);
-
-                    ex.getIn().setBody(String.format("%s %s - %s\n", method, httpRoute, body));
-                })
-                .to("stream:out");
+                .process(log)
+                .to("jms:queue:transations?replyTo=transactions.reply&replyToType=Exclusive");
     }
 }

d71ea234e017c9ef2c4b2b0093b0f2ccd93c1475
Paweł Motyl <pawmot@gmail.com>
2016-10-03 22:12:55 +0200

    Receive and unmarshal Transactions

--- internal-api/src/main/java/com/pawmot/hajsback/internal/api/transactions/QueueNames.java
+++ internal-api/src/main/java/com/pawmot/hajsback/internal/api/transactions/QueueNames.java
@@ -8,4 +8,6 @@ public class QueueNames {
     public static final String ADD_DEBT_QUEUE = "add_debt";
 
     public static final String REPAY_DEBT_QUEUE = "repay_debt";
+
+    public static final String DEBT_GRAPH_QUEUE = "debt_graph";
 }
